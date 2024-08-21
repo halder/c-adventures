d7efa441c186c513e49c683e0bfb8af30be306fe
halder <sascha.halder91@gmail.com>
2024-08-21 17:36:29 +0200

    Undo comment as it was wrong...

--- singly_linked_list.c
+++ singly_linked_list.c
@@ -99,9 +99,7 @@ void linked_list_free(Node *head)
     free(head);
 }
 
-/* Remove last item of linked list by traversal.
- * Note: In order to do this in constant time, we would need a `tail` pointer.
- */
+/* Remove last item of linked list by traversal. */
 void linked_list_remove_tail(Node *head)
 {
     Node *temp = head, *prev = head;
